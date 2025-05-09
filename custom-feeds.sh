bda41730131c7b2496ef23e907d923fb154dff02
phil2sat <phil2sat@gmail.com>
2025-05-09 14:39:26 +0200

    Update custom-feeds.sh

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -4,4 +4,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILD_ROOT/feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILD_ROOT/feeds.conf.default
-echo 'src-git luci_theme_argon https://github.com/jerrykuku/luci-theme-argon' >> $BUILD_ROOT/feeds.conf.default
