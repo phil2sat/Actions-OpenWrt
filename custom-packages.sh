b444c668b55029ef72f86fc43d342f9e3ed00f16
phil2sat <phil2sat@gmail.com>
2025-05-09 14:39:05 +0200

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -2,3 +2,5 @@
 
 #git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
 #mv OpenClash/luci-app-openclash $BUILD_ROOT/package
+git clone https://github.com/jerrykuku/luci-theme-argon
+mv luci-theme-argon $BUILD_ROOT/package
